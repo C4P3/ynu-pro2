c586638df439effcc44753260469a482875abc26
kaito-tsuchi <tsuchido-kaito-vw@ynu.jp>
2025-06-24 13:29:32 +0900

    StartScene ボタン効果音付け足しました

--- Assets/Scripts/MatchButton.cs
+++ Assets/Scripts/MatchButton.cs
@@ -2,12 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
-public class MatchButton : MonoBehaviour, IButton
+public class MatchButton : MonoBehaviour, IButton,IPointerClickHandler,IPointerExitHandler
 {
-    public void OnPointerClick(){
+    [SerializeField] private AudioClip clickSound;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound);
+        }
+        
+        StartCoroutine(LoadSceneWithDelay( 0.3f));
+    }
+
+    private IEnumerator LoadSceneWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
         SceneManager.LoadScene("GameScene");
     }
+
     public void OnPointerEnter(){
 
     }
