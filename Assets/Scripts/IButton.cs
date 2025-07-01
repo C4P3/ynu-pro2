3ab9c614a3fec6d0e2d4d82b93c6f0e36b9b3b89
furufuru810 <furuasyu0415@outlook.jp>
2025-07-01 16:11:37 +0900

    ボタンシェーダー追加

--- Assets/Scripts/IButton.cs
+++ Assets/Scripts/IButton.cs
@@ -1,11 +1,31 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public abstract class IButton : MonoBehaviour
+public class IButton : MonoBehaviour
 {
     [SerializeField] protected Image buttonImage;
     [SerializeField] protected AudioClip clickSound;
     [SerializeField] protected AudioSource audioSource;
+    [SerializeField] protected Color hoverColor = Color.red;
+    [SerializeField] protected Color normalColor = new Color(0, 0, 0, 0); // 透明
+    protected Material matInstance;
+
+    protected void Start()
+    {
+        if (buttonImage == null)
+        {
+            Debug.LogError("targetImageをInspectorで設定してください");
+            return;
+        }
+
+        // マテリアルをインスタンス化して独立制御
+        matInstance = Instantiate(buttonImage.material);
+        buttonImage.material = matInstance;
+
+        matInstance.SetColor("_HoverColor", hoverColor);
+        matInstance.SetColor("_NormalColor", normalColor);
+        matInstance.SetFloat("_IsHover", 0f);
+    }
 
     public virtual void OnPointerClick()
     {
@@ -15,18 +35,33 @@ public abstract class IButton : MonoBehaviour
         }
     }
 
-    public abstract void OnPointerEnter();
+    public virtual void OnPointerEnter(){
+        matInstance.SetFloat("_IsHover", 1f);
+        Debug.Log("排他");
+    }
 
-    public abstract void OnPointerExit();
+    public virtual void OnPointerExit(){
+        matInstance.SetFloat("_IsHover", 0f);
+    }
 
     public virtual void OnPointerDown()
     {
         buttonImage.color = buttonImage.color * 0.7f;
+        if (matInstance != null)
+        {
+            matInstance.SetFloat("_ClickDarkness", 0.7f);
+            matInstance.SetFloat("_ClickSaturation", 1.3f);
+        }
     }
 
     public virtual void OnPointerUp()
     {
         buttonImage.color = buttonImage.color / 0.7f;
+        if (matInstance != null)
+        {
+            matInstance.SetFloat("_ClickDarkness", 1.0f);
+            matInstance.SetFloat("_ClickSaturation", 1.0f);
+        }
     }
 
     public void ChangeUI(CanvasGroup canvasGroup, int alfha, bool interactable, bool blocksRaycasts){//UIの表示非表示
